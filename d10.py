979a1d79bb16f57287b2d08875a530eede2b985b
hyungwoo1413 <glhwguddn@gmail.com>
2025-02-21 17:28:13 +0900

    Update d10.py

--- d10.py
+++ d10.py
@@ -1,17 +1,22 @@
 import random
 import time
 
-# 랜덤으로 8자리 숫자 비밀번호 생성
-password = str(random.randint(0, 99999999)).zfill(8)
+MIN = 00000000
+MAX = 99999999
+
+# 랜덤으로 8자리 숫자 비밀번호 생성 (문자열로 변환)
+password = str(random.randint(MIN, MAX)).zfill(8)
 
 # 실제 비밀번호를 확인하려면 이 줄을 유지합니다. (디버그용)
 print(f"비밀번호는 {password}입니다.")  
+print('찾는 중...')
 
 # 시작 시간 기록
 start_time = time.time()
 
 # 비밀번호를 찾기 위해 00000000부터 99999999까지 확인
-for guess in range(100000000):
+for guess in range(MIN, MAX + 1):
+    # guess 값을 8자리 문자열로 변환
     guess_str = str(guess).zfill(8)
     
     # 비밀번호를 맞추면 시간 측정을 종료하고, 결과 출력
